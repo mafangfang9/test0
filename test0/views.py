9fb2a9bc44473936e899d3610e4da680228e5902
zhangtianqi <a947684132@vip.qq.com>
2018-10-25 10:54:21 +0800

    Merge branch 'master' of github.com:a947684132/test0
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/test0.iml

--- test0/views.py
+++ test0/views.py
@@ -1,12 +1,16 @@
 
 #coding=utf-8
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def goods_view(request):
     return HttpResponse('hello goods!')
 
 
-def user_views(request):
+def user_view(request):
     return HttpResponse('用户登录功能')
 
+
+def article_view(request):
+    return render(request,'article.html')
